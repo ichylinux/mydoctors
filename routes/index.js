0792fe61af3bf4912ae94b4f2cf69d1ec1c61f76
y-matsuda <yuuichi.matsuda@ulsystems.co.jp>
2013-09-03 13:00:50 +0900

    評価の登録

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,10 @@
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function(req, res) {
+  if (req.method == 'POST') {
+    res.render('index', { title: 'My Doctors' });
+  } else {
+    res.render('index', { title: 'My Doctors' });
+  }
 };
