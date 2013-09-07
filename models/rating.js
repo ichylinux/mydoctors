03125292fb7d6f493ad7a972791bd7bfe1c302b2
ichy <ichylinux@gmail.com>
2013-09-07 10:28:13 +0900

    cf

--- models/rating.js
+++ models/rating.js
@@ -1,4 +1,9 @@
-var URL = 'mongodb://127.0.0.1:27017/mydoctors';
+if (process.env.VCAP_SERVICES) {
+  var env = JSON.parse(process.env.VCAP_SERVICES);
+  var url = env['mongolab-n/a'][0]['credentials']['uri'];
+} else {
+  var url = 'mongodb://127.0.0.1:27017/mydoctors';
+}
 
 Rating = function(params) {
   this.hospitalName = params.hospitalName;
@@ -8,9 +13,8 @@ Rating = function(params) {
 
 Rating.prototype.save = function(callback) {
   var $this = this;
-  var client = require('mongodb').MongoClient;
 
-  client.connect(URL, function(err, db) {
+  require('mongodb').connect(url, function(err, db) {
     db.collection('ratings').insert($this, function(err, docs) {
       db.close();
       callback();
@@ -19,8 +23,7 @@ Rating.prototype.save = function(callback) {
 };
 
 Rating.index = function(callback) {
-  var client = require('mongodb').MongoClient;
-  client.connect(URL, function(err, db) {
+  require('mongodb').connect(url, function(err, db) {
     db.collection('ratings').find().toArray(function(err, results) {
       db.close();
       callback(err, results);
